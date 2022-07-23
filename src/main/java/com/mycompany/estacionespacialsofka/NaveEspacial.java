4c1af95fb762f3e7913262f89dda6a50a6ab457c
Alejandro Rueda <alejandrorueda96@hotmail.com>
2022-07-23 11:50:10 -0500

    Getters and Setters NaveEspacial

--- src/main/java/com/mycompany/estacionespacialsofka/NaveEspacial.java
+++ src/main/java/com/mycompany/estacionespacialsofka/NaveEspacial.java
@@ -18,5 +18,55 @@ public class NaveEspacial {
         this.combustible = combustible;
         this.destinoFinal = destinoFinal;
     }
+    //Getters y Setters 
+    public int getIdNave() {
+        return idNave;
+    }
+
+    public void setIdNave(int idNave) {
+        this.idNave = idNave;
+    }
+
+    public String getNombreNave() {
+        return nombreNave;
+    }
+
+    public void setNombreNave(String nombreNave) {
+        this.nombreNave = nombreNave;
+    }
+
+    public String getTipoNave() {
+        return tipoNave;
+    }
+
+    public void setTipoNave(String tipoNave) {
+        this.tipoNave = tipoNave;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+
+    public String getDestinoFinal() {
+        return destinoFinal;
+    }
+
+    public void setDestinoFinal(String destinoFinal) {
+        this.destinoFinal = destinoFinal;
+    }
+    
+    
     
 }
