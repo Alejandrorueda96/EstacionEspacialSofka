12ed18c3a3ea76058c2d1841eb2431ff640d46c7
Alejandro Rueda <alejandrorueda96@hotmail.com>
2022-07-23 11:27:08 -0500

    Constructor NaveEspacial

--- src/main/java/com/mycompany/estacionespacialsofka/NaveEspacial.java
+++ src/main/java/com/mycompany/estacionespacialsofka/NaveEspacial.java
@@ -2,10 +2,21 @@ package com.mycompany.estacionespacialsofka;
 
 
 public class NaveEspacial {
-    int id;
-    String nombre;
+    int idNave;
+    String nombreNave;
     String tipoNave;
     int peso;
     String combustible;
+    String destinoFinal;
+
+    //Constructor NaveEspacial
+    public NaveEspacial(int idNave, String nombreNave, String tipoNave, int peso, String combustible, String destinoFinal) {
+        this.idNave = idNave;
+        this.nombreNave = nombreNave;
+        this.tipoNave = tipoNave;
+        this.peso = peso;
+        this.combustible = combustible;
+        this.destinoFinal = destinoFinal;
+    }
     
 }
